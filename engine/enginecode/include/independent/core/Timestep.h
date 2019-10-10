445e65a60902e72c260c67d49cb8f5f5cd291390
P17215040 <P17215040@my365.dmu.ac.uk>
2019-10-10 14:43:58 +0100

    Just a quick visual

--- engine/enginecode/include/independent/core/Timestep.h
+++ engine/enginecode/include/independent/core/Timestep.h
@@ -4,13 +4,13 @@ namespace Engine {
 
 	class Timestep
 	{
+	private:
+		float m_Time;
 	public:
 		Timestep(float time = 0.0f)
 			: m_Time(time) {}
 
 		float GetSeconds() const { return m_Time; }
 		float GetMilliSeconds() const { return m_Time * 1000.0f; }
-	private:
-		float m_Time;
 	};
 }
