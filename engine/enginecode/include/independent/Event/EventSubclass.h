011a0b83d25e238a48188ea343817586788a8175
P17215040 <P17215040@my365.dmu.ac.uk>
2019-10-15 15:06:34 +0100

    Working on events

--- engine/enginecode/include/independent/Event/EventSubclass.h
+++ engine/enginecode/include/independent/Event/EventSubclass.h
@@ -26,6 +26,7 @@ class WindowCloseEvent : public Event
 {
 public:
 	WindowCloseEvent() {}
+	static EventType getStaticType() { return EventType::WindowClose; }
 
 	EventType getEventType() const override { return EventType::WindowClose; }
 	int getCategoryFlags() const override { return EventCategoryWindow;  }
@@ -39,8 +40,10 @@ private:
 public:
 	WindowResizeEvent(int width, int height) : m_width(width), m_height(height) {}
 
-	EventType getEventType() const override { return EventType::WindowResize; }
-	int getCategoryFlags() const override { return EventCategoryWindow; }
+	static EventType getStaticType() { return EventType::WindowResize; }
+
+	virtual EventType getEventType() const override { return EventType::WindowResize; }
+	virtual int getCategoryFlags() const override { return EventCategoryWindow; }
 
 	inline int getWidth() const { return m_width; }
 	inline int getHeight() const { return m_height; }
