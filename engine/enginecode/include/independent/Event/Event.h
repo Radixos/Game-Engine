05132ca115b88c50aada71bee1b0d939bf4c9910
P17215040 <P17215040@my365.dmu.ac.uk>
2019-10-17 10:34:26 +0100

    working ;)

--- engine/enginecode/include/independent/Event/Event.h
+++ engine/enginecode/include/independent/Event/Event.h
@@ -5,18 +5,39 @@
 #pragma once
 
 //#include "engine.h"
-#include "EventSubclass.h"
 #include <functional>
 #include <iostream>
 
-class Event
+namespace Engine
 {
-private:
-	bool m_handled = false;
-public:
-	virtual EventType getEventType() const = 0;	//!< Get the event type
-	virtual int getCategoryFlags() const = 0;	//!< Get the event category
-	inline bool handled() const { return m_handled; }	//!< Has the event been handled
-	inline void handle(bool isHandled) { m_handled = isHandled; }	//!< Handle the event
-	inline bool isInCategory(EventCategory category) { return getCategoryFlags() & category; }	//!< Is this event in the category?
-};
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		KeyPressed, KeyReleased, KeyTyped,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryWindow = 1 << 0,		// 00000001
+		EventCategoryInput = 1 << 1,		// 00000010
+		EventCategoryKeyboard = 1 << 2,		// 00000100
+		EventCategoryMouse = 1 << 3,		// 00001000
+		EventCategoryMouseButton = 1 << 4	// 00010000
+	};
+
+	class Event
+	{
+	private:
+		bool m_handled = false;
+	public:
+		virtual EventType getEventType() const = 0;	//!< Get the event type
+		virtual int getCategoryFlags() const = 0;	//!< Get the event category
+		inline bool handled() const { return m_handled; }	//!< Has the event been handled
+		inline void handle(bool isHandled) { m_handled = isHandled; }	//!< Handle the event
+		inline bool isInCategory(EventCategory category) { return getCategoryFlags() & category; }	//!< Is this event in the category?
+	};
+
+}
--- engine/enginecode/include/independent/Event/EventSubclass.h
+++ engine/enginecode/include/independent/Event/EventSubclass.h
@@ -4,48 +4,36 @@
 
 #pragma once
 
-enum class EventType
+#include "Event/Event.h"
+
+namespace Engine
 {
-	None = 0,
-	WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
-	KeyPressed, KeyReleased, KeyTyped,
-	MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
-};
 
-enum EventCategory
-{
-	None = 0,
-	EventCategoryWindow = 1 << 0,		// 00000001
-	EventCategoryInput = 1 << 1,		// 00000010
-	EventCategoryKeyboard = 1 << 2,		// 00000100
-	EventCategoryMouse = 1 << 3,		// 00001000
-	EventCategoryMouseButton = 1 << 4	// 00010000
-};
+	class WindowCloseEvent : public Event
+	{
+	public:
+		WindowCloseEvent() {}
+		static EventType getStaticType() { return EventType::WindowClose; }
 
-class WindowCloseEvent : public Event
-{
-public:
-	WindowCloseEvent() {}
-	static EventType getStaticType() { return EventType::WindowClose; }
+		EventType getEventType() const override { return EventType::WindowClose; }
+		int getCategoryFlags() const override { return EventCategoryWindow; }
+	};
 
-	EventType getEventType() const override { return EventType::WindowClose; }
-	int getCategoryFlags() const override { return EventCategoryWindow;  }
-};
+	class WindowResizeEvent : public Event
+	{
+	private:
+		int m_width;
+		int m_height;
+	public:
+		WindowResizeEvent(int width, int height) : m_width(width), m_height(height) {}
 
-class WindowResizeEvent : public Event
-{
-private:
-	int m_width;
-	int m_height;
-public:
-	WindowResizeEvent(int width, int height) : m_width(width), m_height(height) {}
+		static EventType getStaticType() { return EventType::WindowResize; }
 
-	static EventType getStaticType() { return EventType::WindowResize; }
+		virtual EventType getEventType() const override { return EventType::WindowResize; }
+		virtual int getCategoryFlags() const override { return EventCategoryWindow; }
 
-	virtual EventType getEventType() const override { return EventType::WindowResize; }
-	virtual int getCategoryFlags() const override { return EventCategoryWindow; }
+		inline int getWidth() const { return m_width; }
+		inline int getHeight() const { return m_height; }
+	};
 
-	inline int getWidth() const { return m_width; }
-	inline int getHeight() const { return m_height; }
-};
-
+}
