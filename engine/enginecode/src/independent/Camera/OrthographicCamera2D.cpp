3b1f1c8b430dfd2dd4342425c361f54317061991
Radixos <radixomen@gmail.com>
2020-01-05 16:04:39 +0000

    Working

--- engine/enginecode/src/independent/Camera/OrthographicCamera2D.cpp
+++ engine/enginecode/src/independent/Camera/OrthographicCamera2D.cpp
@@ -6,7 +6,6 @@ namespace Engine {
 	void OrthographicCamera2D::updateView()
 	{
 		glm::mat4 translate = glm::translate(glm::mat4(1.f), glm::vec3(m_position, 1.f));
-		//glm::mat4 rotate = glm::rotate(glm::mat4(m_rotation), glm::vec3(0.f, 0.f, 1.f));
 		glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), glm::radians(m_rotation), glm::vec3(0.0f, 0.0f, 1.0f));
 		m_view = translate * rotate;
 
